25c9ddbcb499cb7c837ad4a39c88461de4cf2bd0
reamfox <sashafarion@gmail.com>
2024-10-28 13:17:28 +0200

    innit commit

---
+++ src/test/java/com/amazonagency/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonagency.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
