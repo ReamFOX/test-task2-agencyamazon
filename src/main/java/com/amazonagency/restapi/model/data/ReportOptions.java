85b5e465b872f7b0585974b1baefbb6553b03f3f
reamfox <sashafarion@gmail.com>
2024-10-29 15:01:14 +0200

    remove unnecessary models

--- src/main/java/com/amazonagency/restapi/model/data/ReportOptions.java
+++
@@ -1,9 +0,0 @@
-package com.amazonagency.restapi.model.data;
-
-import lombok.Data;
-
-@Data
-public class ReportOptions {
-    private String dateGranularity;
-    private String asinGranularity;
-}
--- src/main/java/com/amazonagency/restapi/repository/ReportSpecificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.amazonagency.restapi.repository;
-
-import com.amazonagency.restapi.model.data.ReportSpecification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReportSpecificationRepository
-        extends MongoRepository<ReportSpecification, String> {
-}
